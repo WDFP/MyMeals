3526684c12a35bf35bc57ce0447e26597af7e8e6
lynw <c54wang@gmail.com>
2023-04-07 04:25:25 -0400

    adjust positioning

--- src/pages/recipe.js
+++ src/pages/recipe.js
@@ -79,12 +79,9 @@ export default function Recipes({ recipes }) {
 
   return (
     <div>
-      <div class="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16">
-        <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-gray-900">Recipes</h1>
-      </div>
-      <div className="container mx-auto p-4">
-        {/* filter input */}
-
+      <div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16">
+        <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-center text-gray-900 md:text-5xl lg:text-6xl dark:text-gray-900">Recipes</h1>
+        
         <div class="space-y-2 p-4 sticky top-0 z-10 bg-gray-100">
           <span class="text-xs font-medium "> Filters </span>
           <details
@@ -248,8 +245,12 @@ export default function Recipes({ recipes }) {
             </div>
           </details>
         </div>
+        <div className="container mx-auto p-4">
+          {/* filter input */}
 
-        {/* <div className="mb-4">
+ 
+
+          {/* <div className="mb-4">
           <input
             className="border-solid border-2 border-gray-200 p-2 rounded-md w-full"
             type="text"
@@ -293,104 +294,105 @@ export default function Recipes({ recipes }) {
           </div>
         </div> */}
 
-        {/* <!-- recipe card grid--> */}
-        <div className="grid gap-4 gap-y-8 md:grid-cols-2 lg:grid-cols-3 mb-16 z-0">
-          {filteredRecipes.map((recipe) => (
-            <div
-              key={recipe.id}
-              className="flex flex-col justify-between bg-white rounded-md overflow-hidden relative shadow-md"
-            >
-              <div>
-                {/* recipe image */}
-                <Image
-                  className="w-full"
-                  src={recipe.thumbnail_url}
-                  alt={recipe.name}
-                  width={500}
-                  height={500}
-                />
-              </div>
-              <div className="p-4">
-                {/* recipe title */}
-                <h2 className={"line-clamp-1 " + recipeTitle} title={recipe.name}>
-                  {recipe.name}
-                </h2>
-                <div className="flex justify-between mt-4 mb-4 text-gray-500">
-                  <div className="flex items-center">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      className="h-6 w-6"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke="currentColor"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth={2}
-                        d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z"
-                      />
-                    </svg>
-                    {/* cook time */}
-                    <span className="ml-1 lg:text-xl">{recipe.cook_time}</span>
-                  </div>
-                  <div className="flex items-center">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      className="h-5 w-5"
-                      viewBox="0 0 20 20"
-                      fill="currentColor"
-                    >
-                      <path d="M9 2a1 1 0 000 2h2a1 1 0 100-2H9z" />
-                      <path
-                        fillRule="evenodd"
-                        d="M4 5a2 2 0 012-2 3 3 0 003 3h2a3 3 0 003-3 2 2 0 012 2v11a2 2 0 01-2 2H6a2 2 0 01-2-2V5zm3 4a1 1 0 000 2h.01a1 1 0 100-2H7zm3 0a1 1 0 000 2h3a1 1 0 100-2h-3zm-3 4a1 1 0 100 2h.01a1 1 0 100-2H7zm3 0a1 1 0 100 2h3a1 1 0 100-2h-3z"
-                        clipRule="evenodd"
-                      />
-                    </svg>
-                    {/* Number of steps */}
-                    <span className="ml-1 lg:text-xl">#</span>
-                  </div>
-                  <div className="flex items-center">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      className="h-5 w-5"
-                      viewBox="0 0 20 20"
-                      fill="currentColor"
-                    >
-                      <path d="M9 6a3 3 0 11-6 0 3 3 0 016 0zM17 6a3 3 0 11-6 0 3 3 0 016 0zM12.93 17c.046-.327.07-.66.07-1a6.97 6.97 0 00-1.5-4.33A5 5 0 0119 16v1h-6.07zM6 11a5 5 0 015 5v1H1v-1a5 5 0 015-5z" />
-                    </svg>
-                    {/* servings */}
-                    <span className="ml-1 lg:text-xl">{recipe.servings}</span>
-                  </div>
+          {/* <!-- recipe card grid--> */}
+          <div className="grid gap-4 gap-y-8 md:grid-cols-2 lg:grid-cols-3 mb-16 z-0">
+            {filteredRecipes.map((recipe) => (
+              <div
+                key={recipe.id}
+                className="flex flex-col justify-between bg-white rounded-md overflow-hidden relative shadow-md"
+              >
+                <div>
+                  {/* recipe image */}
+                  <Image
+                    className="w-full"
+                    src={recipe.thumbnail_url}
+                    alt={recipe.name}
+                    width={500}
+                    height={500}
+                  />
                 </div>
-                {/* description */}
-                <p
-                  className="mb-3 mt-3 text-gray-500 h-4.5 line-clamp-3"
-                  title={recipe.description}
-                >
-                  {recipe.description}
-                </p>
-                {/* View Recipe Button */}
-                <Link
-                  href={{
-                    pathname: `/recipe/${recipe.slug}`,
-                    query: {
-                      title: recipe.name,
-                      image: recipe.thumbnail_url,
-                      slug: recipe.slug,
-                    },
-                  }}
-                >
-                  <button className={viewRecipeButton}>{viewRecipeLabel}</button>
-                </Link>
+                <div className="p-4">
+                  {/* recipe title */}
+                  <h2 className={"line-clamp-1 " + recipeTitle} title={recipe.name}>
+                    {recipe.name}
+                  </h2>
+                  <div className="flex justify-between mt-4 mb-4 text-gray-500">
+                    <div className="flex items-center">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="h-6 w-6"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          strokeWidth={2}
+                          d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z"
+                        />
+                      </svg>
+                      {/* cook time */}
+                      <span className="ml-1 lg:text-xl">{recipe.cook_time}</span>
+                    </div>
+                    <div className="flex items-center">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="h-5 w-5"
+                        viewBox="0 0 20 20"
+                        fill="currentColor"
+                      >
+                        <path d="M9 2a1 1 0 000 2h2a1 1 0 100-2H9z" />
+                        <path
+                          fillRule="evenodd"
+                          d="M4 5a2 2 0 012-2 3 3 0 003 3h2a3 3 0 003-3 2 2 0 012 2v11a2 2 0 01-2 2H6a2 2 0 01-2-2V5zm3 4a1 1 0 000 2h.01a1 1 0 100-2H7zm3 0a1 1 0 000 2h3a1 1 0 100-2h-3zm-3 4a1 1 0 100 2h.01a1 1 0 100-2H7zm3 0a1 1 0 100 2h3a1 1 0 100-2h-3z"
+                          clipRule="evenodd"
+                        />
+                      </svg>
+                      {/* Number of steps */}
+                      <span className="ml-1 lg:text-xl">#</span>
+                    </div>
+                    <div className="flex items-center">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="h-5 w-5"
+                        viewBox="0 0 20 20"
+                        fill="currentColor"
+                      >
+                        <path d="M9 6a3 3 0 11-6 0 3 3 0 016 0zM17 6a3 3 0 11-6 0 3 3 0 016 0zM12.93 17c.046-.327.07-.66.07-1a6.97 6.97 0 00-1.5-4.33A5 5 0 0119 16v1h-6.07zM6 11a5 5 0 015 5v1H1v-1a5 5 0 015-5z" />
+                      </svg>
+                      {/* servings */}
+                      <span className="ml-1 lg:text-xl">{recipe.servings}</span>
+                    </div>
+                  </div>
+                  {/* description */}
+                  <p
+                    className="mb-3 mt-3 text-gray-500 h-4.5 line-clamp-3"
+                    title={recipe.description}
+                  >
+                    {recipe.description}
+                  </p>
+                  {/* View Recipe Button */}
+                  <Link
+                    href={{
+                      pathname: `/recipe/${recipe.slug}`,
+                      query: {
+                        title: recipe.name,
+                        image: recipe.thumbnail_url,
+                        slug: recipe.slug,
+                      },
+                    }}
+                  >
+                    <button className={viewRecipeButton}>{viewRecipeLabel}</button>
+                  </Link>
+                </div>
+                {/* score tag */}
+                <div className={getScoreTagClass(recipe.score)}>
+                  <span>{getScoreTagLabel(recipe.score)}</span>
+                </div>
               </div>
-              {/* score tag */}
-              <div className={getScoreTagClass(recipe.score)}>
-                <span>{getScoreTagLabel(recipe.score)}</span>
-              </div>
-            </div>
-          ))}
+            ))}
+          </div>
         </div>
       </div>
     </div>
