9e7a8af338129d2ae16c9ba8f4cee55d7827d671
lynw <c54wang@gmail.com>
2023-04-06 21:40:22 -0400

    refactor and add comments

--- src/pages/api/recipes.js
+++ src/pages/api/recipes.js
@@ -2,20 +2,24 @@
 // displays all recipes in JSON format
 import clientPromise from "../../lib/mongodb";
 
+// Define default export function
 export default async(req, res) => {
   try {
-
+    // Connect to MongoDB client
     const client = await clientPromise;
     const db = client.db(process.env.MONGODB_DB);
 
+    // Fetch recipes from MongoDB collection
     const recipes = await db
       .collection("recipes")
       .find({})
       .limit(30)
       .toArray();
 
+    // Send JSON response of fetched data
     res.json(recipes);
-  } catch (e) {
-    console.error(e);
+  } catch (error) {
+    // Handle any error that occurs
+    console.error(error);
   }
 };
