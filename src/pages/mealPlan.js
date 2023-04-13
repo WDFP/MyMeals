581f317f7f88c9c7fc2ee3a3d1321dcf3989738c
Victor Ha <victorha88@gmail.com>
2023-04-13 17:58:37 -0400

    Refactor mealPlan code

--- src/pages/mealPlan.js
+++ src/pages/mealPlan.js
@@ -15,37 +15,8 @@ const daysOfWeek = [
   "Sunday",
 ];
 
-// const recipes = {
-//   Monday: [
-//     { name: "Spaghetti Bolognese", link: "https://example.com/spaghetti-bolognese" },
-//     { name: "Chicken Alfredo", link: "https://example.com/chicken-alfredo" },
-//     { name: "Vegetable Stir-Fry", link: "https://example.com/vegetable-stir-fry" },
-//   ],
-//   Tuesday: [
-//     { name: "Beef Stroganoff", link: "https://example.com/beef-stroganoff" },
-//     { name: "Chicken Curry", link: "https://example.com/chicken-curry" },
-//     { name: "Vegetable Soup", link: "https://example.com/vegetable-soup" },
-//   ],
-//   Wednesday: [
-//     { name: "Lasagna", link: "https://example.com/lasagna" },
-//     { name: "Beef Tacos", link: "https://example.com/beef-tacos" },
-//     { name: "Vegetarian Chili", link: "https://example.com/vegetarian-chili" },
-//   ],
-//   Thursday: [
-//     { name: "Beef Chili", link: "https://example.com/beef-chili" },
-//     { name: "Chicken Fajitas", link: "https://example.com/chicken-fajitas" },
-//     { name: "Vegetarian Enchiladas", link: "https://example.com/vegetarian-enchiladas" },
-//   ],
-//   Friday: [
-//     { name: "Fish and Chips", link: "https://example.com/fish-and-chips" },
-//     { name: "Shrimp Scampi", link: "https://example.com/shrimp-scampi" },
-//     { name: "Vegetarian Pizza", link: "https://example.com/vegetarian-pizza" },
-//   ],
-// };
-
 export default function RecipesPage(props) {
   const [selectedDay, setSelectedDay] = useState("Monday");
-  console.log(props.mealPlans);
   return (
     <div className="flex h-screen bg-zinc-800">
       <NavBar />
@@ -67,18 +38,18 @@ export default function RecipesPage(props) {
           <h2 className="text-green-400 font-bold mb-4">
             {selectedDay}'s Recipes
           </h2>
-          <div className="grid grid-cols-1 gap-4">
+          <div className="flex flex-col items-center w-full">
             {props.mealPlans[0][selectedDay].map((recipe, index) => (
               <Link
                 key={index}
                 href={recipe.link}
-                className="p-4 border-2 border-green-400 rounded-lg text-center hover:bg-green-400 hover:text-white transition-colors duration-300"
+                className="p-4 border-solid hover:border-dotted border-black border-2 rounded-lg text-center hover:text-black transition-colors duration-300 items-center w-full"
               >
                 {recipe.title}
                 <Image
                   src={recipe.image}
-                  width={50}
-                  height={50}
+                  width={150}
+                  height={150}
                   alt={recipe.title}
                 ></Image>
                 <button className="bg-green-400 hover:bg-green-500 text-black font-bold py-2 px-4 rounded mr-4">
