d810c76e37c02912f988ee948bd30014b8fe7ea0
Victor Ha <victorha88@gmail.com>
2023-04-13 19:14:54 -0400

    Implement New NavBar

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -9,21 +9,24 @@ const NavBar = () => {
         </Link>
       </div>
       <div className="flex justify-end items-center w-4/12 pr-4">
-        <form className="mr-4">
-          <input
-            className="rounded-l-lg border-t mr-0 border-b border-l text-gray-800 border-gray-200 bg-white py-2 px-4 block leading-normal w-64"
-            type="text"
-            placeholder="Search"
-          ></input>
-        </form>
-        <Link href="/Login">
+      <Link href="/Login">
           <button className="bg-green-400 hover:bg-green-500 text-white font-bold py-2 px-4 rounded mr-4">
             Login
           </button>
         </Link>
         <Link href="/Register">
-          <button className="bg-green-400 hover:bg-green-500 text-white font-bold py-2 px-4 rounded">
+          <button className="bg-green-400 hover:bg-green-500 text-white font-bold py-2 px-4 rounded mr-4">
             Register
+          </button>
+        </Link>
+        <Link href="/mealPlan">
+          <button className="bg-green-400 hover:bg-green-500 text-white font-bold py-2 px-4 rounded mr-4">
+            Meal Plan
+          </button>
+        </Link>
+        <Link href="/groceryList">
+          <button className="bg-green-400 hover:bg-green-500 text-white font-bold py-2 px-4 rounded">
+            Grocery List
           </button>
         </Link>
       </div>
