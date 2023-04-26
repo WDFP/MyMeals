5b6600ac4908d7bbc42bc9105f61ded207746065
lynw <c54wang@gmail.com>
2023-04-25 22:39:16 -0400

    add nextauth

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -1,19 +1,32 @@
 import Link from "next/link";
+import { useSession, signIn, signOut } from 'next-auth/react';
+
 
 const NavBar = () => {
+  const { data: session } = useSession();
+
+  const handleSignin = (e) => {
+    e.preventDefault();
+    signIn();
+  };
+
+  const handleSignout = (e) => {
+    e.preventDefault();
+    signOut();
+  };
+
   return (
     <nav className='flex pl-4 justify-between items-center bg-white h-16 w-full fixed top-0 z-50'>
       <div className='flex justify-start items-center w-2/6'>
-        <Link href='/Login'>
-          <button className='bg-green-400 hover:bg-green-500 text-white font-bold py-2 px-4 rounded mr-4'>
-            Logged in User
-          </button>
-        </Link>
-        <Link href='/Logout'>
-          <button className='bg-green-400 hover:bg-green-500 text-white font-bold py-2 px-4 rounded mr-4'>
-            Log Out
-          </button>
-        </Link>
+        
+        {!session && <button className='bg-green-400 hover:bg-green-500 text-white font-bold py-2 px-4 rounded mr-4' onClick={handleSignin}>
+          Signin
+        </button>}
+        {session && <button className='bg-green-400 hover:bg-green-500 text-white font-bold py-2 px-4 rounded mr-4' onClick={handleSignout}>
+          Sign out
+        </button>}
+        {session && <Link href='/profile'>{session.user.name}</Link>}
+
       </div>
       <div className='flex justify-center items-center w-2/6'>
         <Link href='/'>
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,7 @@
 import "@/styles/globals.css";
 import React, { useState } from "react";
+import { SessionProvider } from "next-auth/react";
+import '../styles/globals.css';
 
 export default function App({ Component, pageProps }) {
   const [extraRecipeData, setExtraRecipeData] = useState({
@@ -9,10 +11,12 @@ export default function App({ Component, pageProps }) {
   });
 
   return (
-    <Component
-      {...pageProps}
-      extraRecipeData={extraRecipeData}
-      setExtraRecipeData={setExtraRecipeData}
-    />
+    <SessionProvider session={pageProps.session}>
+      <Component
+        {...pageProps}
+        extraRecipeData={extraRecipeData}
+        setExtraRecipeData={setExtraRecipeData}
+      />
+    </SessionProvider>
   );
 }
